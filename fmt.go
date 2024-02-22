828f67e377420e35217e56131325537dc7550b1d
Alfahaaan <ali.farhan160@gmail.com>
2024-02-22 09:44:01 +0700

    package fmt

---
+++ fmt.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	firtsName := "Ali"
+	lastName := "Farhan"
+
+	fmt.Println("Hello '", firtsName, lastName, "'")
+
+	fmt.Printf("Hello '%s %s' \n", firtsName, lastName)
+}

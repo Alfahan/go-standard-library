9fbf9fbe9a81a40e66d3d9dcfe139f9510fa8665
Alfahaaan <ali.farhan160@gmail.com>
2024-02-22 09:52:51 +0700

    package errors

---
+++ errors.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+var (
+	ValidationError = errors.New("validation error")
+	NotFoundError   = errors.New("not found error")
+)
+
+func GetById(id string) error {
+	if id == "" {
+		return ValidationError
+	}
+
+	if id != "alfa" {
+		return NotFoundError
+	}
+
+	// sukses
+	return nil
+}
+
+func main() {
+	err := GetById("huhu")
+	if err != nil {
+		if errors.Is(err, ValidationError) {
+			fmt.Println("validation error")
+		} else if errors.Is(err, NotFoundError) {
+			fmt.Println("not found error")
+		} else {
+			fmt.Println("unknown error")
+		}
+	}
+}
